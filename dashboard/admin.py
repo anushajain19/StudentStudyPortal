18432d63c3e5266dfd7deb73cd0acef23d10f32f
anushajain19 <anusha.jain19401@gmail.com>
2021-11-26 10:00:20 +0530

    initial commit

---
+++ dashboard/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+admin.site.register(Notes)
+admin.site.register(Homework)
+admin.site.register(Todo)
+admin.site.register(Profile)
+admin.site.register(Expense)
---
+++ dashboard/forms.py
@@ -0,0 +1,32 @@
+
+from django import forms
+from . models import *
+from django.contrib.auth.forms import UserCreationForm
+
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ['title', 'description']
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class HomeworkForm(forms.ModelForm):
+    class Meta:
+        model = Homework
+        widgets = {'due':DateInput()}
+        fields = ['subject','title','description','due','is_finished']
+
+class DashboardFom(forms.Form):
+    text = forms.CharField(max_length=100, label="Enter your Search : ")
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title','is_finished']
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','password1','password2']
